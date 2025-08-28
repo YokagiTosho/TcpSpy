a9ac1ef2bec5bca5a0f240ea0296d71a24eab2aa
YokagiTosho <sandascend@gmail.com>
2025-08-29 00:06:05 +0300

    Add another template parameter for AF

--- libTcpSpy/ConnectionsTable.hpp
+++ libTcpSpy/ConnectionsTable.hpp
@@ -27,22 +27,26 @@ private:
     typedef DWORD (__stdcall *GetExtendedUdpTablePtr)(PVOID, PDWORD, BOOL, ULONG, UDP_TABLE_CLASS, ULONG);
 
     using GetExtendedTablePtrs = std::variant<GetExtendedTcpTablePtr, GetExtendedUdpTablePtr>;
+    using TableClasses = std::variant<TCP_TABLE_CLASS, UDP_TABLE_CLASS>;
 
-    template<typename Table, typename TableClass>
+    template<typename Table, typename TableClass, ULONG AddressFamily = AF_INET>
     DWORD _update_table(
-        GetExtendedTablePtrs getExtendedTable, 
-        ProtocolFamily af, 
-        std::variant<TCP_TABLE_CLASS, UDP_TABLE_CLASS> tableClass)
+            GetExtendedTablePtrs getExtendedTable,
+            TableClasses tableClass)
     {
+        if (m_table == nullptr) {
+            alloc_table();
+        }
+
         DWORD dwRes = std::get<Table>(getExtendedTable)
-            (m_table, &m_size, TRUE, (ULONG)af, std::get<TableClass>(tableClass), 0);
+            (m_table, &m_size, TRUE, AddressFamily, std::get<TableClass>(tableClass), 0);
         
         if (dwRes == ERROR_INSUFFICIENT_BUFFER) {
             free_table();
             alloc_table();
 
             dwRes = std::get<Table>(getExtendedTable)
-                (m_table, &m_size, TRUE, (ULONG)af, std::get<TableClass>(tableClass), 0);
+                (m_table, &m_size, TRUE, AddressFamily, std::get<TableClass>(tableClass), 0);
 
             if (dwRes != NO_ERROR) {
                 free_table();
@@ -73,40 +77,44 @@ private:
 
 template<>
 DWORD ConnectionsTable<MIB_TCPTABLE_OWNER_PID, MIB_TCPROW_OWNER_PID>::update_table() {
-    DWORD dwRes = _update_table<GetExtendedTcpTablePtr, TCP_TABLE_CLASS>(
-            GetExtendedTcpTable,
-            ProtocolFamily::INET,
-            TCP_TABLE_OWNER_PID_ALL);
+    DWORD dwRes = _update_table<GetExtendedTcpTablePtr, TCP_TABLE_CLASS>
+        (
+         GetExtendedTcpTable,
+         TCP_TABLE_OWNER_PID_ALL
+        );
 
     return dwRes;
 }
 
 template<>
 DWORD ConnectionsTable<MIB_TCP6TABLE_OWNER_PID, MIB_TCP6ROW_OWNER_PID>::update_table() {
-    DWORD dwRes = _update_table<GetExtendedTcpTablePtr, TCP_TABLE_CLASS>(
-        GetExtendedTcpTable,
-        ProtocolFamily::INET6,
-        TCP_TABLE_OWNER_PID_ALL);
+    DWORD dwRes = _update_table<GetExtendedTcpTablePtr, TCP_TABLE_CLASS, AF_INET6>
+        (
+         GetExtendedTcpTable,
+         TCP_TABLE_OWNER_PID_ALL
+        );
 
     return dwRes;
 }
 
 template<>
 DWORD ConnectionsTable<MIB_UDPTABLE_OWNER_PID, MIB_UDPROW_OWNER_PID>::update_table() {
-    DWORD dwRes = _update_table<GetExtendedUdpTablePtr, UDP_TABLE_CLASS>(
-        GetExtendedUdpTable,
-        ProtocolFamily::INET,
-        UDP_TABLE_OWNER_PID);
+    DWORD dwRes = _update_table<GetExtendedUdpTablePtr, UDP_TABLE_CLASS>
+        (
+         GetExtendedUdpTable,
+         UDP_TABLE_OWNER_PID
+        );
 
     return dwRes;
 }
 
 template<>
 DWORD ConnectionsTable<MIB_UDP6TABLE_OWNER_PID, MIB_UDP6ROW_OWNER_PID>::update_table() {
-    DWORD dwRes = _update_table<GetExtendedUdpTablePtr, UDP_TABLE_CLASS>(
-        GetExtendedUdpTable,
-        ProtocolFamily::INET6,
-        UDP_TABLE_OWNER_PID);
+    DWORD dwRes = _update_table<GetExtendedUdpTablePtr, UDP_TABLE_CLASS, AF_INET6>
+        (
+         GetExtendedUdpTable,
+         UDP_TABLE_OWNER_PID
+        );
 
     return dwRes;
 }
