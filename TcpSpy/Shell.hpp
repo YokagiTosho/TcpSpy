14d1a742f5ce0f6622fde5cdcc227eff31af6b1c
YokagiTosho <sandascend@gmail.com>
2025-09-24 22:34:25 +0300

    Add file path to save picker

--- TcpSpy/Shell.hpp
+++ TcpSpy/Shell.hpp
@@ -1,9 +1,12 @@
 #ifndef SHELL_HPP
 #define SHELL_HPP
 
+#include <commdlg.h>
+#include <shlobj_core.h>
+
 #include "framework.h"
 
-namespace Shell {
+namespace MShell {
 	inline void Properties(HWND hWnd, LPCWSTR lpPath) {
 		SHELLEXECUTEINFO sei{};
 		sei.cbSize = { sizeof(SHELLEXECUTEINFO) };
@@ -13,6 +16,38 @@ namespace Shell {
 		sei.lpFile = lpPath;
 
 		ShellExecuteEx(&sei);
+	}
+
+	inline bool GetSaveFilePath(HWND hWnd, LPWSTR _Inout_ lpFileBuf, int fileBufLen) {
+		OPENFILENAME ofn{};
+		bool ret = true;
+
+		ofn.lStructSize = sizeof(OPENFILENAME);
+		ofn.hwndOwner = hWnd;
+		ofn.lpstrFilter = L".csv";
+		ofn.lpstrCustomFilter = NULL;
+
+		ofn.lpstrFile = lpFileBuf;
+		ofn.nMaxFile = fileBufLen;
+		PWSTR dirPathBuf;
+		if (SHGetKnownFolderPath(FOLDERID_Desktop, 0, NULL, &dirPathBuf) != S_OK) {
+			ret = false;
+			goto exit;
+		}
+
+		ofn.lpstrInitialDir = dirPathBuf;
+		ofn.lpstrTitle = NULL;
+
+		ofn.Flags = OFN_OVERWRITEPROMPT;
+
+		if (!GetSaveFileName(&ofn)) {
+			ret = false;
+			goto exit;
+		}
+
+	exit:
+		CoTaskMemFree(dirPathBuf);
+		return ret;
 	}
 }
 
